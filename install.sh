e40ebd475b5bcb8d9d524f9c993b7f795da9d01c
Shane Curran <s@velodro.me>
2014-02-19 16:39:30 +0000

    Create installer

---
+++ install.sh
@@ -0,0 +1,17 @@
+# B64 console toolkit
+# Allows Base64 encoding and decoding from one command, b64
+# Usage: b64 SXQgd29ya3Mh
+#               -> It works!
+#         b64 "It works!"
+#               -> SXQgd29ya3Mh
+#
+# @author Shane Curran
+
+echo "*** Installing Simple-Base64 ***"
+mkdir ~/.b64
+wget https://raw.github.com/ShaneCurran/Simple-Base64/master/b64.php -O ~/.b64/b64.php
+echo "b64 () {
+          php ~/.b64/b64.php "$1"
+}" > ~/.bashrc
+source ~/.bashrc
+echo "*** Install Complete ***"
